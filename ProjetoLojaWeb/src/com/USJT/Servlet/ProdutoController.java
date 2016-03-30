2debb0bfe4cb8eb491c47f5aa2bcb61857c7921d
FernandaRachel <fefedel9@hotmail.com>
2016-03-30 15:49:12 -0300

    Update and rename ProjetoLojaWeb/src/com/usjt/Servlet/ProdutoController.java to ProjetoLojaWeb/src/com/USJT/Servlet/ProdutoController.java
    Strings, instancias e métodos adicionados, necessarios para classe JSP

--- ProjetoLojaWeb/src/com/USJT/Servlet/ProdutoController.java
+++ ProjetoLojaWeb/src/com/USJT/Servlet/ProdutoController.java
@@ -3,6 +3,7 @@ package com.usjt.Servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.usjt.ProjetoSJ.Produto;
+import com.usjt.TO.ProdutoTO;
 
 @WebServlet("/ManterProduto.do")
 public class ProdutoController extends HttpServlet {
@@ -19,31 +21,29 @@ public class ProdutoController extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String pId = request.getParameter("id");
 		String pQtd = request.getParameter("qtd");
 		String pValor = request.getParameter("valor");
 		String pNome = request.getParameter("nome");
 		String pMarca = request.getParameter("marca");
 		String pForn = request.getParameter("fornecedor");
-		int id = Integer.parseInt(pId);
+		
 		double valor = Double.parseDouble(pValor);
 		int qtd = Integer.parseInt(pQtd);
 		String event = request.getParameter("event");
-		
-		Produto p = new Produto(id, qtd, valor, pNome, pMarca, pForn);
-		
+
+		Produto p = new Produto(0, qtd, valor, pNome, pMarca, pForn);
+
 		if (event.equals("Adicionar Produto")) {
-		
+
 			p.criar();
 
 			PrintWriter out = response.getWriter();
@@ -54,10 +54,10 @@ public class ProdutoController extends HttpServlet {
 			out.println("marca: " + p.getMarca() + "<br>");
 			out.println("fornecedor: " + p.getFornecedor() + "<br>");
 			out.println("</body></html>");
-		}
-		else if(event.equals("Excluir Produto")){
+		} else if (event.equals("Excluir Produto")) {
+			int id = Integer.parseInt(pId);
 			p.delete();
-			
+
 			PrintWriter out = response.getWriter();
 			out.println("<html><head><title>Produto Cadastrado</title></head><body>");
 			out.println("id: " + p.getId() + "<br>");
@@ -67,9 +67,9 @@ public class ProdutoController extends HttpServlet {
 			out.println("fornecedor: " + p.getFornecedor() + "<br>");
 			out.println("</body></html>");
 
-		} else if(event.equals("Atualizar")){
+		} else if (event.equals("Atualizar")) {
 			p.atualizar();
-			
+
 			PrintWriter out = response.getWriter();
 			out.println("<html><head><title>Produto Cadastrado</title></head><body>");
 			out.println("id: " + p.getId() + "<br>");
@@ -79,9 +79,9 @@ public class ProdutoController extends HttpServlet {
 			out.println("fornecedor: " + p.getFornecedor() + "<br>");
 			out.println("</body></html>");
 
-		}else {
+		} else {
 			p.carregar();
-			
+
 			PrintWriter out = response.getWriter();
 			out.println("<html><head><title>Produto Cadastrado</title></head><body>");
 			out.println("id: " + p.getId() + "<br>");
@@ -91,6 +91,20 @@ public class ProdutoController extends HttpServlet {
 			out.println("fornecedor: " + p.getFornecedor() + "<br>");
 			out.println("</body></html>");
 		}
+
+		ProdutoTO to = new ProdutoTO();
+		to.setId(p.getId());
+		to.setQtd(p.getQtd());
+		to.setValor(p.getValor());
+		to.setNome(p.getNome());
+		to.setMarca(p.getMarca());
+		to.setFornecedor(p.getFornecedor());
+		
+
+		request.setAttribute("produto", to);
+
+		RequestDispatcher view = request.getRequestDispatcher("Produto.jsp");
+		view.forward(request, response);
 
 	}
 
