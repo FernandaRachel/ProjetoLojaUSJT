3fc8084b09a576c63184b84e0fc92165519939a4
FernandaRachel <fefedel9@hotmail.com>
2016-03-30 15:46:22 -0300

    Update and rename ProjetoLojaWeb/src/com/usjt/Servlet/FuncionarioControle.java to ProjetoLojaWeb/src/com/USJT/Servlet/FuncionarioControle.java
    Métodos, Strings e Instanciação de classe  necessarios para JSP adicionados

--- ProjetoLojaWeb/src/com/USJT/Servlet/FuncionarioControle.java
+++ ProjetoLojaWeb/src/com/USJT/Servlet/FuncionarioControle.java
@@ -1,15 +1,20 @@
 package com.usjt.Servlet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.usjt.ProjetoSJ.Funcionario;
+import com.usjt.TO.FuncionarioTO;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class FuncionarioControle {
+@WebServlet("/FuncionarioController.do")
+public class FuncionarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -24,10 +29,11 @@ public class FuncionarioControle {
 		String pUser = request.getParameter("user");
 		String pSenha = request.getParameter("senha");
 		String pRG = request.getParameter("rg");
-		int id = Integer.parseInt(pID);
+		
 		String event = request.getParameter("event");
 
-		Funcionario f = new Funcionario();
+		
+		Funcionario f = new Funcionario(0,pUser,pSenha,pRG);
 
 		if (event.equals("Cadastrar")) {
 			f.criar();
@@ -35,7 +41,10 @@ public class FuncionarioControle {
 			PrintWriter out = response.getWriter();
 			out.println("Cliente cadastrado");
 			
+			f.carregar();
 		} else if (event.equals("Excluir")) {
+			int id = Integer.parseInt(pID);
+			
 			f.excluir();
 			
 			PrintWriter out = response.getWriter();
@@ -69,5 +78,20 @@ public class FuncionarioControle {
 			out.println("</body></html>");
 
 		}
+		f.carregar();
+		
+		FuncionarioTO to = new FuncionarioTO();
+		to.setIdFuncionario(f.getIdFuncionario());
+		to.setUsuario(f.getUsuario());
+		to.setSenha(f.getSenha());
+		to.setRg(f.getRg());
+		
+		request.setAttribute("funcionario", to);
+
+		RequestDispatcher view = 
+					request.getRequestDispatcher("Funcionario.jsp");
+		view.forward(request, response);
+
+		
 	}
 }
